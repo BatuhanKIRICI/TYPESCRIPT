0df467984737e66df330c4715a5f7ec6d6e3fd2a
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-05 15:49:31 +0300

    "Array Data-type"

---
+++ app.js
@@ -0,0 +1,7 @@
+// let values: (string | number | boolean)[] = [1, 89, "jack", "david", true];
+var values = [1, 89, "jack", "david", true];
+// const value: any = values[4];
+// console.log(value);
+for (var i = 0; i < values.length; i++) {
+    console.log(values[i]);
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
+// let values: (string | number | boolean)[] = [1, 89, "jack", "david", true];
+let values: Array<string | number | boolean> = [1, 89, "jack", "david", true];
 
+// const value: any = values[4];
+
+// console.log(value);
+
+for (let i = 0; i < values.length; i++) {
+  console.log(values[i]);
+}
