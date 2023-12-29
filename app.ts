67a29253caa69847d8e5e46f2556ae6990329ac4
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-29 22:27:33 +0300

    "Axios example"

--- app.ts
+++ app.ts
@@ -1,3 +1,26 @@
-const a: string = "Hello";
+import axios from "axios";
 
-console.log(a);
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The todo with id: ${id}
+    Has a title of: ${title}
+    Is it completed? ${completed}`);
+};
