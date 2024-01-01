2caff39f793ff8f87c43143db272218c957e77f6
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-01 21:23:56 +0300

    "Destructuring"

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -24,3 +24,28 @@ const throwError = (message: string): string => {
   }
   return message;
 };
+
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+/* const logWeather = (forecast: { date: Date; weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+}; */
+
+// logWeather(todaysWeather);
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
