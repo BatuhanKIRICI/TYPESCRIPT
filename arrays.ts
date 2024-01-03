b09ea5c045a7af4c8ffa09e41fc43298154d1720
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-03 14:45:01 +0300

    "Types in arrays"

--- arrays.ts
+++ arrays.ts
@@ -1,4 +1,15 @@
-const carMakers: (string | number)[] = ["toyota", "bmw", "ford", 1];
+const carMakers: string[] = ["toyota", "bmw", "ford"];
 const dates: Date[] = [new Date(), new Date()];
 
 const carsByMake: string[][] = [["corolla"], ["525i"], ["focus"]];
+
+carMakers.push(100);
+
+carMakers.map((car: string): string => {
+  return car;
+});
+
+const importantDate: (Date | string)[] = [new Date()];
+importantDate.push("2027-08-08");
+importantDate.push(new Date());
+importantDate.push(61616);
