2b48c329e8adc042c478987d0aaa09a304dd7cfc
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-03 16:47:04 +0300

    "Code reuse"

--- tuples.ts
+++ tuples.ts
@@ -1,12 +1,12 @@
-const drink = {
+/* const drink = {
   color: "brown",
   carbonated: true,
   sugar: 50,
-};
+}; */
 
 // const beverage: [string, boolean, number] = ["brown", true, 50];
 
-type Drink = [string, boolean, number]; //Type alias
+/* type Drink = [string, boolean, number]; //Type alias
 
 const beverage: Drink = ["brown", true, 50];
-const beverage2: Drink = ["white", false, 88];
+const beverage2: Drink = ["white", false, 88]; */
