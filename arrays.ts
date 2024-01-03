ab0bd51661701891cf69be80ac238d96b797efd5
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-03 14:01:25 +0300

    "Arrays"

---
+++ arrays.ts
@@ -0,0 +1,4 @@
+const carMakers: (string | number)[] = ["toyota", "bmw", "ford", 1];
+const dates: Date[] = [new Date(), new Date()];
+
+const carsByMake: string[][] = [["corolla"], ["525i"], ["focus"]];
